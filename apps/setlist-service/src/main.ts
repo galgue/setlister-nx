ec28f19e3a365b7edf56f8435db217208df00463
Gal Gueta <galg@cynet.com>
2024-02-10 17:45:38 +0200

    add dayjs and cookie parser

--- apps/setlist-service/src/main.ts
+++ apps/setlist-service/src/main.ts
@@ -5,11 +5,19 @@
 
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app/app.module';
 
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import dayjs from 'dayjs';
+
+dayjs.extend(customParseFormat);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;

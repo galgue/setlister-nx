3ce103cd25db91668d12bab6dc8531763ea74df0
Gal Gueta <galg@cynet.com>
2024-02-17 23:24:58 +0200

    add queue without caching mechanism

--- apps/setlist-service/src/services/setlist-fm-api/setlist-fm-api.service.ts
+++ apps/setlist-service/src/services/setlist-fm-api/setlist-fm-api.service.ts
@@ -10,6 +10,7 @@ import {
 } from './setlist-fm-api.consumers';
 import dayjs from 'dayjs';
 import { GetArtistResponseDto } from './dtos/get-artist-response.dto';
+import { SetlistSetlistFmDto } from './dtos/setlist-setlist-fm.dto';
 
 @Injectable()
 export class SetlistFmApiService {
@@ -17,6 +18,30 @@ export class SetlistFmApiService {
     @InjectQueue('setlist') private readonly setlistQueue: Queue,
     @Inject(CACHE_MANAGER) private cacheService: Cache
   ) {}
+
+  private async runQueueJob<T>(
+    queueName: SetlistProcessType,
+    queueData: SetlistProcessDataType<typeof queueName>,
+    options: { priority: number }
+  ) {
+    const thisJob = await this.setlistQueue.add(queueName, queueData, {
+      priority: options.priority,
+    });
+
+    return await new Promise<T>((resolve, reject) => {
+      this.setlistQueue.on('completed', async (job, result: T) => {
+        if (job.id === thisJob.id) {
+          resolve(result);
+        }
+      });
+
+      this.setlistQueue.on('failed', (job, err) => {
+        if (job.id === thisJob.id) {
+          reject(err);
+        }
+      });
+    });
+  }
 
   private async getFromCacheOrRunQueue<T>(
     key: string,
@@ -30,30 +55,8 @@ export class SetlistFmApiService {
       return res;
     }
 
-    const thisJob = await this.setlistQueue.add(queueName, queueData, {
+    return await this.runQueueJob<T>(queueName, queueData, {
       priority: options.priority,
-    });
-
-    return await new Promise<T>((resolve, reject) => {
-      this.setlistQueue.on('completed', async (job, result: T) => {
-        if (job.id === thisJob.id) {
-          this.cacheService.set(
-            key,
-            result,
-            typeof options.ttl === 'function'
-              ? options.ttl(result)
-              : options.ttl
-          );
-
-          resolve(result);
-        }
-      });
-
-      this.setlistQueue.on('failed', (job, err) => {
-        if (job.id === thisJob.id) {
-          reject(err);
-        }
-      });
     });
   }
 
@@ -74,33 +77,36 @@ export class SetlistFmApiService {
   }
 
   async getArtistSetlists(mbid: string, priority: 'sync' | 'async' = 'sync') {
-    const data =
-      await this.getFromCacheOrRunQueue<GetArtistSetlistsResponseDto>(
-        `artist-setlists:${mbid}`,
-        'getArtistSetlists',
-        {
-          mbid,
-          page: 1,
-        } as SetlistProcessDataType<'getArtistSetlists'>,
-        {
-          priority: priority === 'sync' ? 1 : 2,
-          ttl: (result) => {
-            const today = dayjs();
-            const nextShow = result.setlist
-              .filter((setlist) =>
-                dayjs(setlist.eventDate, 'DD-MM-YYYY').isAfter(today)
-              )
-              .sort((a, b) =>
-                dayjs(a.eventDate, 'DD-MM-YYYY').diff(
-                  dayjs(b.eventDate, 'DD-MM-YYYY')
-                )
-              )[0];
-            return nextShow
-              ? dayjs(nextShow.eventDate, 'DD-MM-YYYY').diff(today)
-              : 7 * 24 * 60 * 60 * 1000; // 7 days
-          },
+    const data = await this.runQueueJob<GetArtistSetlistsResponseDto>(
+      'getArtistSetlists',
+      {
+        mbid,
+        page: 1,
+      } as SetlistProcessDataType<'getArtistSetlists'>,
+      {
+        priority: priority === 'sync' ? 1 : 2,
+      }
+    );
+
+    const nextShow = data.setlist
+      .filter((setlist) =>
+        dayjs(setlist.eventDate, 'DD-MM-YYYY').isAfter(dayjs())
+      )
+      .reduce((acc, setlist) => {
+        if (acc === null) {
+          return setlist;
         }
-      );
+
+        if (
+          dayjs(setlist.eventDate, 'DD-MM-YYYY').isBefore(
+            dayjs(acc.eventDate, 'DD-MM-YYYY')
+          )
+        ) {
+          return setlist;
+        }
+
+        return acc;
+      }, null as SetlistSetlistFmDto | null);
 
     const first10PastShows = data.setlist
       .filter(
@@ -108,9 +114,23 @@ export class SetlistFmApiService {
           dayjs(setlist.eventDate, 'DD-MM-YYYY').isBefore(dayjs()) &&
           setlist.sets.set.length > 0
       )
+      .map((setlist) => ({
+        ...setlist,
+        sets: {
+          set: setlist.sets.set.map((set) => ({
+            ...set,
+            song: set.song.filter((song) => !song.tape),
+          })),
+        },
+      }))
       .slice(0, 10);
 
-    return first10PastShows;
+    return {
+      first10PastShows,
+      nextShow: nextShow?.eventDate
+        ? dayjs(nextShow.eventDate, 'DD-MM-YYYY').toDate()
+        : null,
+    };
   }
 
   async getArtist(mbid: string) {

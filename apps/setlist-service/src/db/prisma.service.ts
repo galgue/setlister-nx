6069f8e517d48c85d0eff18c72040ac2a2cdb309
Gal Gueta <galg@cynet.com>
2024-02-27 16:24:07 +0200

    Add computed fields for songsCalculated and needRecalculate

--- apps/setlist-service/src/db/prisma.service.ts
+++ apps/setlist-service/src/db/prisma.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import dayjs from 'dayjs';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
@@ -8,5 +9,29 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   }
   async onModuleInit() {
     await this.$connect();
+
+    this.$extends({
+      result: {
+        artist: {
+          songsCalculated: {
+            needs: {
+              nextShow: true,
+              noNextShow: true,
+            },
+            compute(data) {
+              return data.nextShow || data.noNextShow;
+            },
+          },
+          needRecalculate: {
+            needs: {
+              nextShow: true,
+            },
+            compute(data) {
+              return dayjs(data.nextShow).isBefore(dayjs());
+            },
+          },
+        },
+      },
+    });
   }
 }
